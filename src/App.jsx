f2215c492c25cbde7b44ecb5177ee622d0f9c8f3
Gaurav anand <Gauravanand1016@gmail.com>
2026-06-07 15:40:59 +0530

    Add Notes App source code

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react"
+
+const App = () => {
+
+  const [title, setTitle] = useState('')
+  const [details,SetDetails] = useState('')
+  const [task,setTask] = useState([])
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+
+    const copyTask =[...task];
+    copyTask.push({title,details})
+
+    setTask(copyTask);
+
+    setTitle(' ')
+    SetDetails(' ')
+  }
+  const deleteNote = (idx)=>{
+    const copyTask = [...task];
+
+    copyTask.splice(idx,1)
+
+    setTask(copyTask)
+    
+
+  }
+  
+  
+  return (
+    <div className='h-screen lg:flex bg-black text-white'>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}className='flex gap-4 lg:w-1/2 p-10 flex-col item-start'>
+        <h1 className="text-4xl font-bold">Add Notes</h1>  
+
+        {/*PAHLA INPUT FOR HEADING*/}
+          <input 
+            type="text"
+            placeholder='Enter Your Notes Heading' 
+            className='px-5 py-2 w-full font-medium outline-none border-2 rounded'
+            value={title}
+            onChange={(e)=>{
+              setTitle(e.target.value)
+            }}
+          />
+
+        {/* DETAILED WALA INPUT */}
+          <textarea 
+            type="text"
+            className='px-5 py-2 h-32 w-full 
+            font-medium outline-none  border-2 rounded'
+            placeholder='Write Details here'
+            value={details}
+            onChange={(e)=>{
+              SetDetails(e.target.value)
+            }}
+          />
+
+          <button className='bg-white active:scale-95 text-black w-full font-medium  px-5 py-2 outline-none  rounded'>Add Note</button>
+        
+
+      </form>
+      <div className="lg:w-1/2 lg:border-l-2 p-10">
+        <h1 className='text-4xl font-bold'> Recent Notes</h1>
+        <div className="flex flex-wrap items-start justify-start gap-5 mt-5 h-[90%] overflow-auto">
+          {task.map(function(elem,idx){
+
+            return <div key={idx} className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]">
+               <div>
+                <h3 className='leading-tight text-lg font-bold'>{elem.title}</h3>
+                <p className='mt-2 leading-tight text-xs font-semibold text-gray-600'>{elem.details}</p>
+              </div>
+              <button onClick={() => {
+                deleteNote(idx)
+              }} className='w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white'>Delete</button>
+            </div>
+
+          })}
+          
+        </div>
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
